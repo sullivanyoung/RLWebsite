5286aa521fd73cd8b8580786e412458b4cba5d2c
Sullivan Young <sullivangyoung@gmail.com>
2020-11-05 23:04:31 -0500

    import project

---
+++ RocketLeague/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RocketLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RLEsports()
+        {
+            ViewBag.Message = "This page is dedicated to all things Rocket League";
+
+            return View();
+        }
+
+        public ActionResult Liquipedia()
+        {
+            ViewBag.Message = "Page dedicated to RL Players and their statistics";
+
+            return View();
+        }
+    }
+}
