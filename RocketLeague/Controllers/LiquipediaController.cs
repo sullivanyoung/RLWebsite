a0eccdd5ef863316d6fbcdf662fde7454e68f024
Sullivan Young <sullivangyoung@gmail.com>
2020-11-10 16:57:34 -0500

    added content

---
+++ RocketLeague/Controllers/LiquipediaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RocketLeague.Controllers
+{
+    public class LiquipediaController : Controller
+    {
+        public ActionResult Teams()
+        {
+            return View();
+        }
+        public ActionResult Player()
+        {
+            return View();
+        }
+    }
+}
