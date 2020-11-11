971d3e554dd89d33d676bff6ba801981a8c11089
Sullivan Young <sullivangyoung@gmail.com>
2020-11-11 09:13:15 -0500

    add player model/update images

--- RocketLeague/Controllers/LiquipediaController.cs
+++ RocketLeague/Controllers/LiquipediaController.cs
@@ -14,6 +14,7 @@ namespace RocketLeague.Controllers
         }
         public ActionResult Player()
         {
+            ViewBag.Message = "Player Page";
             return View();
         }
     }
---
+++ RocketLeague/Models/PlayerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RocketLeague.Models
+{
+    public class PlayerModel
+    {
+        []
+        public int playerId { get; set; }
+        []
+        public string playerName { get; set; }
+        public string  playerBirthDate { get; set; }
+        public string  playerTeam { get; set; }
+        public double playerEarnings { get; set; }
+
+    }
+}
